90e499983054cff35146813e963371fbd45f6037
EslamFouda7 <eslamamer311@gmail.com>
2025-12-01 16:49:56 +0200

    edit

--- main.js
+++ main.js
@@ -3,24 +3,28 @@ function changeImg() {
     const leftImg = document.querySelector('.img-left');
     const centerImg = document.querySelector('.img-center');
     const rightImg = document.querySelector('.img-right');
-    
-   
-        leftImg.className = 'img-center';
-        centerImg.className = 'img-right';
-        rightImg.className = 'img-left';
-    }
-   
 
- function rotate() {
-        circle.classList.remove('rotate-left', 'rotate-right');
-        if (Math.random() > 0.5) {
-            circle.classList.add('rotate-right');
 
-        }
-        else {
-            circle.classList.add('rotate-left');
-        }
+    leftImg.className = 'img-center';
+    centerImg.className = 'img-right';
+    rightImg.className = 'img-left';
+}
+
+let count = 0;
+function rotate() {
+
+    circle.classList.remove('rotate-left', 'rotate-right');
+    void circle.offsetWidth; // Trigger reflow to restart animation
+    if (count === 0) {
+        circle.classList.add('rotate-right');
+        count = 1;
+
     }
+    else {
+        circle.classList.add('rotate-left');
+        count = 0;
+    }
+}
 
 setInterval(changeImg, 3000);
 setInterval(rotate, 3000);
